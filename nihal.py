2e31fc100534c70f2c72e7c26d20bf0862360b41
nihal-musthafa <111424074+nihal-musthafa@users.noreply.github.com>
2022-08-17 14:43:21 +0530

    first commit

---
+++ nihal.py
@@ -0,0 +1,12 @@
+def max_of_three(a,b,c):
+    if a > b and a > c:
+        return a
+    elif b > a and b > c:
+        return b
+    else:
+        return c
+
+
+
+sum=max_of_three(22,2,3)
+print(sum)
