5a7271a5b036e4aa144cbc67c529af86163d1642
nihal-musthafa <111424074+nihal-musthafa@users.noreply.github.com>
2023-02-22 14:04:00 +0530

    Update nihal.py

--- nihal.py
+++ nihal.py
@@ -1,12 +1,27 @@
-def max_of_three(a,b,c):
-    if a > b and a > c:
-        return a
-    elif b > a and b > c:
-        return b
-    else:
-        return c
+<h2 class="bg-primary">profile</h1>
+    <div class="row">
+   <div class="col">
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="{{request.user.p_user.profile_pic.url}}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">{{request.user.first_name}} {{request.user.last_name}}</h5>
+          <p class="card-text">i am {{request.user.p_user.age}} years old</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"> gender:{{request.user.p_user.gender}}</li>
+          <li class="list-group-item"> email: {{request.user.p_user.email}}</li>
+          <li class="list-group-item">phone: {{request.user.p_user.phone}}</li>
+        </ul>
+        <div class="card-body">
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>
 
-
-
-sum=max_of_three(22,2,3)
-print(sum)
+   </div>
+   
+   <div class="col-4" style="border-left:2px solid black;height: 650px;">
+    <a href="{% url 'addpro' %}" class="btn btn-warning">Add Profile</a>
+</div>
+   </div>
+{% endblock nav %}
